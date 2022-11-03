69e9077785acb32a9f76aea096b478aada228b7b
Steel <Steelmen87@bk.ru>
2022-11-03 12:39:02 +0300

    main

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import HW14 from "../../../q1-work/HW14";
+import Main from "../../../q1-work/HW14";
 
 function App() {
     return (
         <div className="App">
-            <HW14/>
+            <Main/>
         </div>
     );
 }
